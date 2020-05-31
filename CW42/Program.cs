e35933e7e6e903f138e075cc5d0812dd53ddbefe
Andrew <amostachov@gmail.com>
2020-05-31 23:51:09 +0300

    Ready for upload

--- CW42/Program.cs
+++ CW42/Program.cs
@@ -13,9 +13,13 @@ namespace CW42
 {
     class Program
     {
+        // Ну директория тут, ну шо.
         const string directory = "../../../Книги";
+
+        // Ну и такой вот путь (туть).
         const string uri = "https://www.gutenberg.org/files/1342/1342-0.txt";
 
+        // Словарь соответствия.
         static private readonly Dictionary<char, string> Letters = new Dictionary<char, string>
         {
             ['A'] = "А",
@@ -78,21 +82,30 @@ namespace CW42
 
             MultiThread();
 
-            GetFileFromWeb(uri, "../../../");
+            GetAndProcess(uri, directory);
         }
 
+        /// <summary>
+        /// Чтение, транслитерация и запись файла.
+        /// </summary>
+        /// <param name="filename">Путь к файлу</param>
+        /// <param name="pairs">Словаарь соответствия</param>
         static void ReadReplaceAndWrite(string filename, Dictionary<char, string> pairs)
         {
+            // Получаем тут непосредственное имя файла.
             string text = "";
 
             string[] pathsplit = filename.Split(new char[] { '/', '\\' });
             string thename = pathsplit.Last();
 
+            // Количество изначальных символов.
             int initialnum = 0;
 
+            // Читаем-с.
             try
             {
                 text = File.ReadAllText(filename);
+                // Записываем количество символов.
                 initialnum = text.Length;
             }
             catch (Exception e) when (e is IOException || e is System.Security.SecurityException || e is AccessViolationException)
@@ -100,8 +113,10 @@ namespace CW42
                 Console.WriteLine($"Error while reading file {filename}");
             }
 
+            // Транслитерируем.
             text = Translit(text, pairs);
 
+            // Записываем в новый файл.
             try
             {
                 pathsplit[pathsplit.Length - 1] = "new_" + pathsplit[pathsplit.Length - 1];
@@ -112,15 +127,21 @@ namespace CW42
                 Console.WriteLine($"Error while Writing file");
             }
 
+            // Выводим нужную инфу.
             Console.WriteLine($"File {thename} went from {initialnum} to {text.Length} characters.");
         }
 
+        /// <summary>
+        /// Обычная, последовательная обработка файлов в директории.
+        /// </summary>
         static void Conventional()
         {
+            // Таймер для подсчета времени.
             Stopwatch stopwatch = new Stopwatch();
 
             stopwatch.Start();
 
+            // Транслитим каждый файл.
             try
             {
                 foreach (var file in Directory.GetFiles(directory))
@@ -136,16 +157,23 @@ namespace CW42
                 Console.WriteLine("Please help");
             }
 
+            // Останавливаем таймер, выводим инфу.
             stopwatch.Stop();
 
             Console.WriteLine($"Files processed consequently in {stopwatch.ElapsedMilliseconds / (double) 1000} seconds.\n");
         }
 
+        /// <summary>
+        /// Параллелизированная обработка файлов.
+        /// </summary>
         static void MultiThread()
         {
+            // Лист задач обработки.
             List<Task> tasks = new List<Task>();
 
             Stopwatch stopwatch = new Stopwatch();
+
+            //Добавляем таски для каждого файла.
             try
             {
                 foreach (var file in Directory.GetFiles(directory))
@@ -161,6 +189,7 @@ namespace CW42
                 Console.WriteLine("Please help");
             }
 
+            // Делаем общий таск, который подождет, пока завершится все внутри, и запускаем.
             Task MegaTask = Task.WhenAll(tasks);
 
             stopwatch.Start();
@@ -169,11 +198,18 @@ namespace CW42
 
             stopwatch.Stop();
 
+            // Выводим инфу.
             Console.WriteLine($"Files processed asynchronously in {stopwatch.ElapsedMilliseconds / (double) 1000} seconds.\n");
         }
 
-        static void GetFileFromWeb(string url, string path)
+        /// <summary>
+        /// Получение файла по ссылке и обработка.
+        /// </summary>
+        /// <param name="url">Ссылка на страницу</param>
+        /// <param name="dir">Директория записи</param>
+        static void GetAndProcess(string url, string dir)
         {
+            // Реквест по ссылке.
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             request.Method = "GET";
 
@@ -181,6 +217,7 @@ namespace CW42
             string responsefromserver = "";
             bool ifsuccessful = true;
 
+            // Отправляем запрос, получаем ответ, profit.
             try
             {
                 WebResponse response = request.GetResponse();
@@ -192,7 +229,6 @@ namespace CW42
                     StreamReader reader = new StreamReader(dataStream);
                     responsefromserver = reader.ReadToEnd();
                 }
-                Console.WriteLine("Finished!");
             }
             catch (Exception)
             {
@@ -202,31 +238,47 @@ namespace CW42
 
             Stopwatch stopwatch = new Stopwatch();
 
+            // Если успешно скачали, запускаем обработ(очку).
             if (ifsuccessful)
             {
                 stopwatch.Start();
 
+                int initialsymbols = responsefromserver.Length;
+
                 string text = Translit(responsefromserver, Letters);
 
+                // Записываем готовый файл.
                 try
                 {
-                    File.WriteAllText(directory + "/new_book_from_web.txt", text);
+                    File.WriteAllText(dir + "/new_book_from_web.txt", text);
                 }
                 catch (Exception e) when (e is IOException || e is System.Security.SecurityException || e is AccessViolationException)
                 {
                     Console.WriteLine($"Error while Writing file");
                 }
 
+                Console.WriteLine($"Downloaded book went from {initialsymbols} to {text.Length} characters.");
+
                 stopwatch.Stop();
             }
 
+            // Выводим инфу.
             Console.WriteLine($"Downloaded book was processed in {stopwatch.ElapsedMilliseconds / (double) 1000} seconds.");
         }
 
+        /// <summary>
+        /// Транслитерация текста.
+        /// </summary>
+        /// <param name="text">Изначальный текст</param>
+        /// <param name="pairs">Словарь соответствия</param>
+        /// <returns></returns>
         static string Translit(string text, Dictionary<char, string> pairs)
         {
+            // Создаем список строк.
             List<string> textlist = new List<string>();
 
+            // Пробегаем по данному тексту, проверяем, является ли чар буквой,
+            // Проверяем, есть ли в словаре, заменяем (или нет).
             for (int i = 0; i < text.Length; i++)
             {
                 char current = text[i];
@@ -244,6 +296,7 @@ namespace CW42
                 }
             }
 
+            // Сгоняем все в строку, возвращаем.
             return string.Join("", textlist.ToArray());
         }
     }
